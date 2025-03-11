4a1fced05489dc0b3f4db4ddc7a5f5dba3637611
Michela Martini <martini.michela02@gmail.com>
2025-03-11 20:18:26 +0100

    added first things with labs

---
+++ lab/lab01-node/ex0.mjs
@@ -0,0 +1,12 @@
+function ex0(strings) {
+    for (let str of strings) {
+        if (str.length < 2)
+            console.log("")
+        else if (str.length === 2)
+            console.log(str + str)
+        else
+            console.log(str.slice(0, 2) + str.slice(str.length - 2, str.length))
+    }
+}
+
+ex0(["spring", "it", "cat"])
